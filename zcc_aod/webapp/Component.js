051c28a3e5a5c8a68f479c8dbfb1105b26d2bebc
Ioana Enescu <ioana.enescu@msg.group>
2026-08-18 11:38:25 +0300

    Created Admin Overview Dashboard App and card for incidents

---
+++ zcc_aod/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zccaod.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
